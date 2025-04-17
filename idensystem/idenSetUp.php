1f99e995dc7e43dc6024c7967be8841a5d960dc5
sergiosmam-dev <sergiomartinalarconmontero@gmail.com>
2025-04-17 16:58:40 -0400

    Archivo de Configuración
    Carga del archivo de configuración, por seguridad se eliminaron los datos de acceso y fueron remplazados por *

---
+++ idensystem/idenSetUp.php
@@ -0,0 +1,25 @@
+<?php
+define('DEFAULT_URL_HTACCESS','vibnorcaiden');
+
+define('DEFAULT_CONTROLLER','index');/* CONTROLADOR POR DEFECTO DE LA APLICACION */
+define('BASE_VIEW_URL','https://ibnorca.org/');/* RUTA POR DEFECTO DE LA VISTAS */
+//define('BASE_VIEW_URL','http://localhost/ibnorca/');/* RUTA POR DEFECTO DE LA VISTAS */
+define('DEFAULT_VIEW_LAYOUT','assets');/* VISTA POR DEFECTO DEL FRAMEWORK IDEN*/
+
+define('DEFAULT_SESSION_USER_TIME',30);
+define('DEFAULT_HASH_KEY','*****************');//
+
+define('DEFAULT_USER_REGISTER','user');
+define('DEFAULT_USER_AUTHENTICATE','**************');
+
+define('DEFAULT_DB_HOST','***********');
+define('DEFAULT_DB_ROOT_USER','*********');
+define('DEFAULT_DB_ROOT_PASS','*********');
+define('DEFAULT_DB_NAME','*********');
+
+/*define('DEFAULT_DB_ROOT_USER','**************');
+define('DEFAULT_DB_ROOT_PASS','***************');
+define('DEFAULT_DB_NAME','************');*/
+
+define('DEFAULT_DB_CHARSET','utf8');
+?>
